1473ec769d8792dce739fa3f0ec788777ba751b6
renan.yuhara <renan.yuhara@neomind.com.br>
2019-12-17 14:15:30 -0300

    Implementado justify-content e wrap

--- source-code/main.js
+++ source-code/main.js
@@ -1,29 +1,108 @@
 $(document).ready(() => {
 
+    const setActiveButton = (id) => {
+        $('input[type="button"]').removeClass('active');
+        $(id).addClass('active');
+    }
+
     const FLEX_ROW_CLASS = 'flex-row';
     const FLEX_ROW_REVERSE_CLASS = 'flex-row-reverse';
     const FLEX_COLUMN_CLASS = 'flex-column';
     const FLEX_COLUMN_REVERSE_CLASS = 'flex-column-reverse';
 
-    const removeAllAndAddClass = (chosenClass) => {
+    const FLEX_WRAP_NO_WRAP = 'flex-wrap-nowrap';
+    const FLEX_WRAP_WRAP = 'flex-wrap-wrap';
+    const FLEX_WRAP_WRAP_REVERSE = 'flex-wrap-wrap-reverse';
+
+    const addFlexClass = (chosenClass) => {
+        $('.content').addClass(chosenClass);
+    }
+
+    const removeAllFlexDirectionAndAddClass = (chosenClass) => {
         $('.content').removeClass(FLEX_ROW_CLASS);
         $('.content').removeClass(FLEX_ROW_REVERSE_CLASS);
         $('.content').removeClass(FLEX_COLUMN_CLASS);
         $('.content').removeClass(FLEX_COLUMN_REVERSE_CLASS);
 
+        addFlexClass(chosenClass);
+    }
+
+    const removeAllFlexWrap = () => {
+        $('.content').removeClass(FLEX_WRAP_NO_WRAP);
+        $('.content').removeClass(FLEX_WRAP_WRAP);
+        $('.content').removeClass(FLEX_WRAP_WRAP_REVERSE);
+    }
+    
+
+
+    $('#bt-row').on('click', () => {
+        removeAllFlexDirectionAndAddClass(FLEX_ROW_CLASS);
+    });
+    $('#bt-row-reverse').on('click', () => {
+        removeAllFlexDirectionAndAddClass(FLEX_ROW_REVERSE_CLASS);
+    });
+    $('#bt-column').on('click', () => {
+        removeAllFlexDirectionAndAddClass(FLEX_COLUMN_CLASS);
+    });
+    $('#bt-column-reverse').on('click', () => {
+        removeAllFlexDirectionAndAddClass(FLEX_COLUMN_REVERSE_CLASS);
+    });
+
+    $('#bt-wrap-nowrap').on('click', () => {
+        removeAllFlexWrap();
+        addFlexClass(FLEX_WRAP_NO_WRAP);
+    });
+
+    $('#bt-wrap-wrap').on('click', () => {
+        removeAllFlexWrap();
+        addFlexClass(FLEX_WRAP_WRAP);
+    });
+
+    $('#bt-wrap-wrap-reverse').on('click', () => {
+        removeAllFlexWrap();
+        addFlexClass(FLEX_WRAP_WRAP_REVERSE);
+    });
+    
+    const FLEX_JUSTIFY_FLEX_START = 'flex-justify-flex-start';
+    const FLEX_JUSTIFY_FLEX_END = 'flex-justify-flex-end';
+    const FLEX_JUSTIFY_CENTER = 'flex-justify-center';
+    const FLEX_JUSTIFY_SPACE_BETWEEN = 'flex-justify-space-between';
+    const FLEX_JUSTIFY_SPACE_AROUND = 'flex-justify-space-around';
+    const FLEX_JUSTIFY_SPACE_EVENLY = 'flex-justify-space-evenly';
+
+    const removeAllFlexJustifyAndAddClass = (chosenClass) => {
+        $('.content').removeClass(FLEX_JUSTIFY_FLEX_START);
+        $('.content').removeClass(FLEX_JUSTIFY_FLEX_END);
+        $('.content').removeClass(FLEX_JUSTIFY_CENTER);
+        $('.content').removeClass(FLEX_JUSTIFY_SPACE_BETWEEN);
+        $('.content').removeClass(FLEX_JUSTIFY_SPACE_AROUND);
+        $('.content').removeClass(FLEX_JUSTIFY_SPACE_EVENLY);
+
         $('.content').addClass(chosenClass);
     }
 
-    $('#bt-row').on('click', () => {
-        removeAllAndAddClass(FLEX_ROW_CLASS);
-    });
-    $('#bt-row-reverse').on('click', () => {
-        removeAllAndAddClass(FLEX_ROW_REVERSE_CLASS);
-    });
-    $('#bt-column').on('click', () => {
-        removeAllAndAddClass(FLEX_COLUMN_CLASS);
-    });
-    $('#bt-column-reverse').on('click', () => {
-        removeAllAndAddClass(FLEX_COLUMN_REVERSE_CLASS);
-    });
+    $('#flex-justify-flex-start').on('click', () => {
+        removeAllFlexJustifyAndAddClass(FLEX_JUSTIFY_FLEX_START);
+        setActiveButton('#flex-justify-flex-start');
+    })
+    $('#flex-justify-flex-end').on('click', () => {
+        removeAllFlexJustifyAndAddClass(FLEX_JUSTIFY_FLEX_END);
+        setActiveButton('#flex-justify-flex-end');
+    })
+    $('#flex-justify-center').on('click', () => {
+        removeAllFlexJustifyAndAddClass(FLEX_JUSTIFY_CENTER);
+        setActiveButton('#flex-justify-center');
+    })
+    $('#flex-justify-space-between').on('click', () => {
+        removeAllFlexJustifyAndAddClass(FLEX_JUSTIFY_SPACE_BETWEEN);
+        setActiveButton('#flex-justify-space-between');
+    })
+    $('#flex-justify-space-around').on('click', () => {
+        removeAllFlexJustifyAndAddClass(FLEX_JUSTIFY_SPACE_AROUND);
+        setActiveButton('#flex-justify-space-around');
+    })
+    $('#flex-justify-space-evenly').on('click', () => {
+        removeAllFlexJustifyAndAddClass(FLEX_JUSTIFY_SPACE_EVENLY);
+        setActiveButton('#flex-justify-space-evenly');
+    })
 });
